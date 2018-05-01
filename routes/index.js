16cde9064336d500308813add257adddc120ca90
rodrigo gutierrez <warrock225@gmail.com>
2018-05-01 13:54:53 +0200

    Add cors in routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 'use strict'
 
 const express = require('express')
+const cors = require('cors')
 const carCtrl = require('../controllers/car')
 const userCtrl = require('../controllers/user')
 const auth = require('../middlewares/auth')
 const api = express.Router()
 
-api.get('/car', carCtrl.getCars)
-api.get('/car/:carId', carCtrl.getCar)
-api.post('/car', carCtrl.saveCar)
-api.put('/car/:carId', carCtrl.updateCar)
-api.delete('/car/:carId', carCtrl.deleteCar)
-api.post('/signup', userCtrl.signUp)
-api.post('/signin', userCtrl.signIn)
-api.get('/private', auth, (req, res) => {
+api.get('/car',cors(), carCtrl.getCars)
+api.get('/car/:carId',cors(), carCtrl.getCar)
+api.post('/car',cors(), carCtrl.saveCar)
+api.put('/car/:carId',cors(), carCtrl.updateCar)
+api.delete('/car/:carId',cors(), carCtrl.deleteCar)
+api.post('/signup',cors(), userCtrl.signUp)
+api.post('/signin',cors(), userCtrl.signIn)
+api.get('/private', cors(),auth, (req, res) => {
     res.status(200).send({ message: 'Tienes acceso' })
 })
 
